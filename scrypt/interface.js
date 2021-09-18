1d5b7982e33834bce8d82948dde2e113f2b039e5
Sandro <sandrox377@gmail.com>
2021-09-18 15:21:29 -0300

    criando bombas e dinamica de detonação

--- scrypt/interface.js
+++ scrypt/interface.js
@@ -4,17 +4,50 @@
 // 2 - se hoiver apenas ao redor o quadradro tera o numero que o cercam , 
 // 3 - caso esteja vazio ele mostrara outros quadradros vazios.
 const field = document.getElementById("field")
+let rowLegth = 10
+let zonelegth = 80
+let mina = 10 
 
+initializeGame()
+
+
+
+// função para iniciar o jogo
 function initializeGame(){
-    let zonelegth = 80
+    // criação do campo minado com as zonas(quadrados)
     for(i = 0; i < zonelegth; i++){
      let zone = document.createElement("div");
      zone.className = "zone"
+     zone.id = i
      field.appendChild(zone)
-
-
+    
 
     }
+    // adicionando as minas
+    creatMinas ()
+    
+
 
 }
-initializeGame()
+
+function creatMinas (){
+    //  bsuca um numero aleatori e coloca a class mina e o evento para detonar a bomba
+    for(i=0; i < mina; i++){
+       let randomNumber = Math.round(Math.random() * zonelegth)
+       let element = document.getElementById(randomNumber)
+       element.className = "mina"
+       element.addEventListener("click" , gameOver)
+       
+    }
+}
+
+function gameOver (){
+    let minas = document.querySelectorAll("div.mina")
+    minas.forEach(element => {
+        element.className = "detona"
+        
+    });
+
+
+
+}
